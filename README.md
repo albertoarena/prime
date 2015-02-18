5b41be733106c434e24479a22fb2a120a81a11bc
Alberto Arena <arena.alberto@gmail.com>
2015-02-18 11:53:21 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# prime
+This Javascript library is designed to generate a list of prime numbers.
+
+It supports also negative prime numbers, that strictly speaking shouldn't exist but that are often needed in some
+applications.
+
+## How to use it
+
+    /**
+     * Get a list of prime numbers
+     *
+     * @param {number} start                    start, can be negative
+     * @param {number} count                    count of primes returned
+     * @param {number|undefined} maxPrime       max prime, only if start is positive
+     * @returns {Array}
+     */
+
+Create a list of prime numbers
+
+    # First 10 prime numbers from 0
+    prime(0, 10)
+    # ==> [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+    # First 10 prime numbers from 2
+    prime(0, 10)
+    # ==> [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+    # First 5 prime numbers from -10
+    prime(-10, 5)
+    # ==> [-7, -5, -3, -2, 2]
+
+You can specify the maximum prime number allowed:
+
+    # First 10 prime numbers from 0, but not greater than 20
+    prime(0, 10, 20)
+    # ==> [2, 3, 5, 7, 11, 13, 17, 19]
+
+If the maximum prime is negative, it will be ignored.
---
+++ src/prime.js
@@ -0,0 +1,61 @@
+/**
+ * Getter/Setter
+ *
+ * @author Alberto Arena <arena.alberto@gmail.com>
+ * @licence MIT https://github.com/albertoarena/gettersetter/blob/master/LICENSE-MIT
+ *
+ */
+define([], function () {
+
+    function isPrime(number) {
+        var start = 2;
+        while (start <= Math.sqrt(number)) {
+            if (number % start++ < 1) return false;
+        }
+        return number > 1;
+    }
+
+    /**
+     * Get a list of prime numbers
+     *
+     * @param {number} start                    start, can be negative
+     * @param {number} count                    count of primes returned
+     * @param {number|undefined} maxPrime       max prime, only if start is positive
+     * @returns {Array}
+     */
+    function getPrimes(start, count, maxPrime) {
+        var primes = [];
+        var num = start;
+
+        if (start < 0) {
+            primes = getPrimes(2, count, Math.abs(start)).map(function (n) {
+                return n * -1;
+            }).reverse();
+            if (primes.length >= count) {
+                return primes;
+            }
+            else {
+                num = 2;
+            }
+        }
+
+        // Remove maxMinPrime if it's negative
+        if (maxPrime !== undefined && maxPrime < 0) {
+            maxPrime = undefined;
+        }
+
+        while (primes.length < count) {
+            if (isPrime(num)) {
+                if (maxPrime !== undefined && num > maxPrime) {
+                    break;
+                }
+                primes.push(num);
+            }
+            num++;
+        }
+
+        return primes;
+    }
+
+    return getPrimes;
+});
